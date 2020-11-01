c2f8ba4c2bea7c94ab1788b053bf4922b44f9902
Muthu-Git <lmalamu1998@gmail.com>
2020-11-02 01:35:27 +0530

    admin login added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminhomeComponent } from './adminhome/adminhome.component';
+
 import { NgohomeComponent } from './ngohome/ngohome.component';
 import{StephomeComponent} from './stephome/stephome.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
 
 
 const routes: Routes = [
   {path: '',component: StephomeComponent},
   {path: 'stephomeLink',component:StephomeComponent},
   {path:'ngohomeLink', component:NgohomeComponent},
-  {path:'adminhomeLink',component:AdminhomeComponent}
+  {path:'adminloginLink',component:AdminloginComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { StephomeComponent } from './stephome/stephome.component';
 import { NgohomeComponent } from './ngohome/ngohome.component';
 import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     StephomeComponent,
     NgohomeComponent,
-    AdminhomeComponent
+    AdminhomeComponent,
+    AdminloginComponent
+ 
   ],
   imports: [
     BrowserModule,
