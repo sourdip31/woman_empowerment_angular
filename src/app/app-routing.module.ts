47286976e7ec484243527f4792fea53aafa16bcd
sourdip31 <bubun.souradip@gmail.com>
2020-11-07 13:56:51 +0530

    change in routing module and app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { NgohomeComponent } from './ngohome/ngohome.component';
 
 import{StephomeComponent} from './stephome/stephome.component';
-<<<<<<< HEAD
 import { UserRegisterComponent } from './user-register/user-register.component';
 import { UserloginComponent } from './userlogin/userlogin.component';
-=======
-import { AdminloginComponent } from './adminlogin/adminlogin.component';
-
-import { NgohomeComponent } from './ngohome/ngohome.component';
-import { NgologinComponent } from './ngologin/ngologin.component';
-
-
->>>>>>> 5d80d3532ea45a9eefb3e1605fb25d8c16417707
+import {NgologinComponent} from './ngologin/ngologin.component' ;
+import {AdminloginComponent} from './adminlogin/adminlogin.component' ;
 
 
 const routes: Routes = [
   {path: '',component: StephomeComponent},
-<<<<<<< HEAD
   {path:'stephomeLink', component: StephomeComponent},
   {path: 'userloginLink', component: UserloginComponent},
   {path:'ngohomeLink', component:NgohomeComponent},
   {path:'adminhomeLink',component:AdminhomeComponent},
-  {path: 'userRegister',component: UserRegisterComponent}
-
-=======
-  {path: 'stephomeLink',component:StephomeComponent},
   {path: 'ngohomeLink', component:NgohomeComponent},
   {path:'adminloginLink',component:AdminloginComponent},
   {path:'ngologinLink',component:NgologinComponent},
+  {path: 'userRegister',component: UserRegisterComponent}
+
   
->>>>>>> 5d80d3532ea45a9eefb3e1605fb25d8c16417707
 ];
 
 @NgModule({
