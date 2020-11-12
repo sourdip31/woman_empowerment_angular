ba866603366411deb19cbdc965f6bf38113f5a6c
Muthu-Git <lmalamu1998@gmail.com>
2020-11-13 03:04:16 +0530

    Final commit

--- src/app/ngodeletecourse/ngodeletecourse.component.ts
+++ src/app/ngodeletecourse/ngodeletecourse.component.ts
@@ -1,28 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {Ngodeletecourse} from '../ngodeletecourse';
-import{Router} from '@angular/router';
-import{NgodeletecourseService} from '../ngodeletecourse.service';
+import { Ngodeletecourse } from '../ngodeletecourse';
+import { Router } from '@angular/router';
+import { NgodeletecourseService } from '../ngodeletecourse.service';
 @Component({
   selector: 'app-ngodeletecourse',
   templateUrl: './ngodeletecourse.component.html',
   styleUrls: ['./ngodeletecourse.component.css']
 })
 export class NgodeletecourseComponent implements OnInit {
-
-  delcourse : Ngodeletecourse = new Ngodeletecourse() ;
-  constructor(private ngodeletecourseService : NgodeletecourseService, private router:Router ) { }
+  message: string;
+  delcourse: Ngodeletecourse = new Ngodeletecourse();
+  constructor(private ngodeletecourseService: NgodeletecourseService, private router: Router) { }
 
   ngOnInit(): void {
   }
-  deletecourse(){
+  deletecourse() {
     //alert(JSON.stringify(this.customer));
     this.ngodeletecourseService.ngodeletecourse(this.delcourse).subscribe(data => {
       //alert(JSON.stringify(data));
-      if(data.status == 'SUCCESS') {
-        this.router.navigate(['addcoursesLink'])
+      if (data.status == 'SUCCESS') {
+        this.router.navigate(['addcoursesLink']);
       }
       else {
         //this.router.navigate(['addcoursesLink'])
+        this.message=data.message;
       }
     })
   }
